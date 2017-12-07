d302eea72cc747b6ec2cf0928757ff76741ca9db
walkingalchemy <walkingalchemy@gmail.com>
2017-12-07 15:08:33 -0500

    Refactor methods into Committee and Congressman models

--- lib/models/committee.rb
+++ lib/models/committee.rb
@@ -1,4 +1,33 @@
 class Committee < ActiveRecord::Base
   has_many :committee_members
   has_many :congressmen, class_name: "Congressman", through: :committee_members
+
+  def self.info_name(name)
+    committee = Committee.find_by(name: name)
+    puts "#{committee.name}"
+    puts "Committee Chair: #{committee.chair}"
+    puts "Committee Members:"
+    members = CommitteeMember.select {|cm| cm.committee_id == committee.id}
+    members.each do |m|
+      Congressman.find(m.id).full_name
+    end
+  end
+
+  def self.list
+    self.all.each_with_index do |committee, index|
+      puts "#{index + 1}. #{committee.abbreviation}  -  #{committee.name}"
+    end
+  end
+
+
+  def self.info_initials(initials)
+    committee = Committee.find_by(abbreviation: initials)
+    puts "#{committee.name}"
+    puts "Committee Chair: #{committee.chair}"
+    puts "Committee Members:"
+    members = CommitteeMember.select {|cm| cm.committee_id == committee.id}
+    members.each do |m|
+      Congressman.find(m.id).full_name
+    end
+  end
 end
--- lib/models/congressman.rb
+++ lib/models/congressman.rb
@@ -8,8 +8,44 @@ class Congressman < ActiveRecord::Base
     puts "#{self.short_title} #{self.first_name} #{self.middle_name} #{self.last_name} (#{self.party.abbreviation})"
   end
 
+  def self.lookup(full_name_input)
+    name_array = full_name_input.split
+    f_name = name_array[0]
+    l_name = name_array[-1]
+    Congressman.find_by(first_name: f_name, last_name: l_name)
+  end
 
+  def self.basic_info(full_name_input)
+    selected = self.lookup(full_name_input)
+    selected_id = Party.find(selected.party_id).abbreviation
+    output =
+     <<-OUTPUT
+       Basic Information
+       #{selected.short_title} #{selected.first_name} #{selected.middle_name} #{selected.last_name}
+       (#{selected_id})
+       Next election: #{selected.next_election}
 
+       Contact Information and Social Media
+       Office : #{selected.office_address}
+       Phone : #{selected.phone}
+       Fax : #{selected.fax}
+       Twitter : #{selected.twitter_account}
+       Facebook : #{selected.facebook_account}
+       Youtube : #{selected.youtube_account}
+       Website : #{selected.url}
+       Email Link : #{selected.contact_form}
+                    (Copy into browser)
+     OUTPUT
+    puts output
+  end
+
+  def self.committees(full_name_input)
+    selected = self.lookup(full_name_input)
+    selected_comms = selected.committees
+    selected_comms.each_with_index do |committee, index|
+      puts "#{index + 1}. #{committee.abbreviation}  -  #{committee.name}"
+    end
+  end
 
 
 
